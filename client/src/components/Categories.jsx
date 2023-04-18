b96fc48bdd5d8f0547880b1a1e3353eb91d6d6e9
RameesAP <rameeswork@gmail.com>
2023-04-19 01:38:43 +0530

    cate

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+import CategoriesItems from './CategoriesItems'
+import { CatgoriesTempData } from '../data'
+
 
 const Categories = () => {
-  return (
-    <Container>Categories</Container>
-  )
+    return (
+        <Container>
+            {CatgoriesTempData.map(item => (
+                <CategoriesItems item={item} />
+            ))}
+        </Container>
+    )
 }
-
+const Container = styled.div`
+    display: flex;
+    padding: 20px;
+    justify-content: space-between;
+`
 export default Categories
--- client/src/data.js
+++ client/src/data.js
@@ -23,5 +23,63 @@ export const SliderTempData = [
 ]
 
 export const CatgoriesTempData = [
+    {
+        id:1,
+        img:"https://images.pexels.com/photos/5693889/pexels-photo-5693889.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title:"KIDS WEAR"
+    },
+    {
+        id:2,
+        img:"https://images.pexels.com/photos/757316/pexels-photo-757316.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title:"MENS WEAR"
+    },
+    {
+        id:3,
+        img:"https://images.pexels.com/photos/3119215/pexels-photo-3119215.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title:"WOMENS WEAR"
+    },
+]
+
+export const popularProducts=[
+    {
+        id:1,
+        img:"https://images.pexels.com/photos/904117/pexels-photo-904117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id:2,
+        img:"https://images.pexels.com/photos/1141212/pexels-photo-1141212.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id:3,
+        img:"https://images.pexels.com/photos/1139448/pexels-photo-1139448.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id:4,
+        img:"https://images.pexels.com/photos/1141215/pexels-photo-1141215.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id:5,
+        img:"https://images.pexels.com/photos/1193945/pexels-photo-1193945.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id:6,
+        img:"https://images.pexels.com/photos/13310288/pexels-photo-13310288.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id:7,
+        img:"https://images.pexels.com/photos/5525005/pexels-photo-5525005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id:8,
+        img:"https://images.pexels.com/photos/9521669/pexels-photo-9521669.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id:9,
+        img:"https://images.pexels.com/photos/6071513/pexels-photo-6071513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id:10,
+        img:"https://images.pexels.com/photos/6071508/pexels-photo-6071508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
     
 ]
