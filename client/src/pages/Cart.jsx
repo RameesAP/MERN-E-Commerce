c0bc9be2de4491787b27620c75288524d5c571aa
RameesAP <rameeswork@gmail.com>
2023-04-27 01:32:34 +0530

    cartitem

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import Navbar from '../components/Navbar'
 import Announcement from '../components/Announcement'
 import Footer from '../components/Footer'
+import { Add, Remove } from '@material-ui/icons'
 
 const Cart = () => {
     return (
@@ -23,10 +24,42 @@ const Cart = () => {
                     <Info>
                         <Product>
                             <ProductDetails>
-                                <Image src="https://images.pexels.com/photos/3270223/pexels-photo-3270223.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
-                                <Details></Details>
+                                <Image src="https://images.pexels.com/photos/2529147/pexels-photo-2529147.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+                                <Details>
+                                    <ProductName><b>Product:</b> Pine Forest</ProductName>
+                                    <ProductId><b>ID:</b> 888855</ProductId>
+                                    <ProductColor color="white" />
+                                    <ProductSize><b>Size:</b> 37.5</ProductSize>
+                                </Details>
                             </ProductDetails>
-                            <PriceDetails></PriceDetails>
+                            <PriceDetails>
+                                <ProductAmountContainer>
+                                    <Add />
+                                    <ProductAmount>2</ProductAmount>
+                                    <Remove />
+                                </ProductAmountContainer>
+                                <ProductPrice>₹ 2000</ProductPrice>
+                            </PriceDetails>
+                        </Product>
+                        <Hr />
+                        <Product>
+                            <ProductDetails>
+                                <Image src="https://images.pexels.com/photos/3270223/pexels-photo-3270223.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+                                <Details>
+                                    <ProductName><b>Product:</b> Pine Forest</ProductName>
+                                    <ProductId><b>ID:</b> 888855</ProductId>
+                                    <ProductColor color="black" />
+                                    <ProductSize><b>Size:</b> 37.5</ProductSize>
+                                </Details>
+                            </ProductDetails>
+                            <PriceDetails>
+                                <ProductAmountContainer>
+                                    <Add />
+                                    <ProductAmount>2</ProductAmount>
+                                    <Remove />
+                                </ProductAmountContainer>
+                                <ProductPrice>₹ 2000</ProductPrice>
+                            </PriceDetails>
                         </Product>
                     </Info>
                     <Summary>Summary</Summary>
@@ -78,23 +111,70 @@ const Bottom = styled.div`
 const Info = styled.div`
     flex: 3;
 `
+const Hr = styled.hr`
+    background-color: #eee;
+    border: none;
+    height: 1px;
+`
+
 const Summary = styled.div`
     flex: 1;
 `
 const Product = styled.div`
-    
+    display: flex;
+    justify-content: space-between;
 `
 const ProductDetails = styled.div`
+    flex: 2;
+    display: flex;
+`
+const Image = styled.img`
+    width: 200px;
+    height: 200px;
+ 
+
+`
+
+const Details = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`
+const ProductName = styled.span`
     
 `
-const Image = styled.div`
+const ProductId = styled.span`
+    
+`
+const ProductColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props => props.color};
+
+`
+const ProductSize = styled.span`
     
 `
 const PriceDetails = styled.div`
-    
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 `
-const Details = styled.div`
-    
+const ProductAmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
 `
-
+const ProductAmount = styled.div`
+    font-size: 24px;
+    margin: 5px;
+`
+const ProductPrice = styled.div`
+    font-size: 30px;
+    font-weight: 200;
+`
 export default Cart
