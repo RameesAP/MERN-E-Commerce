3b676a5291aaaf8c7e98e48513d4c1182d1e41a8
RameesAP <rameeswork@gmail.com>
2023-05-01 01:28:10 +0530

    db error finder

--- server/index.js
+++ server/index.js
@@ -19,9 +19,8 @@ app.use('/',userRoutes)
 
 
 //db connection
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>app.listen(process.env.PORT || 5000,()=>{
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>app.listen(process.env.PORT,()=>{
     console.log(`server listening at ${process.env.PORT}`);
-}))
-.catch((error)=>{console.log(error);})
+})).catch((error)=>{console.log(error)})
 
