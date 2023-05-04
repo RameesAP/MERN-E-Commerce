c5d6fd22eb7e3aaf7e753b6c022defd66d5e013a
RameesAP <rameeswork@gmail.com>
2023-05-04 20:16:21 +0530

    user Completed

--- server/Routes/user.js
+++ server/Routes/user.js
@@ -1,11 +1,18 @@
 import express from 'express'
 import { verifyTokenAndAdmin, verifyTokenAndAuthorization } from '../controllers/verifyTokenControllers.js';
-import { updateUser } from '../controllers/userControllers.js';
+import { deleteUser, getAllUser, getUser, updateUser, userStats } from '../controllers/userControllers.js';
 
 
 const router=express.Router();
 
 router.put('/:id',verifyTokenAndAuthorization,updateUser,verifyTokenAndAdmin)
 
+router.delete('/:id',verifyTokenAndAuthorization,deleteUser)
+
+router.get('/find/:id',verifyTokenAndAdmin,getUser)
+
+router.get('/users',verifyTokenAndAdmin,getAllUser)
+
+router.get("/stats",verifyTokenAndAdmin,userStats)
 
 export default router
